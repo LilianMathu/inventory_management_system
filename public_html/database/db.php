e6c6fa4331daf871017b0cb39df50c6e6d9e3283
LilianMathu <lilianmathu2018@gmail.com>
2021-07-27 14:52:27 +0300

    setup the database

---
+++ public_html/database/db.php
@@ -0,0 +1,28 @@
+<?php 
+
+class Database 
+{
+            private $con;
+
+        public function connect() {
+
+            include_once("constants.php");
+
+            $this->con = new mysqli(HOST, USER, PASS, DB);
+            
+            if ($this->con) {
+                echo "connected";
+
+                return $this->con;
+
+            } else {
+                return "Database connection failed";
+            }
+
+        } 
+}
+
+// $db = new Database();
+// $db->connect();
+
+?>
