7135762d183af0ab7e07facaa89e395443a0ce3d
LilianMathu <lilianmathu2018@gmail.com>
2021-07-27 14:52:57 +0300

    transfer jquery to js folder

--- public_html/index.php
+++ public_html/index.php
@@ -15,7 +15,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
- 
+    <script type="text/javascript" src="../public_html/js//main.js"></script>
 
 </head>
 
@@ -24,14 +24,6 @@
     <div class="jumbotron">
         <h1>Hello world</h1>
     </div>
-
-    <!-- js -->
-    <script type="text/javascript">
-    // test if jquery is working
-        $(document).ready(function() {
-            alert("Hello friends");
-        })
-    </script>
 
 </body>
 
