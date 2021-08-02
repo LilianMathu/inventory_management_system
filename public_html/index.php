1d1dd88512cbd7ae4b24ed27c4d12ebd461d650c
LilianMathu <lilianmathu2018@gmail.com>
2021-08-03 00:05:23 +0300

    add login form

--- public_html/index.php
+++ public_html/index.php
@@ -33,6 +33,36 @@
       
     ?>
 
+    <!-- Login -->
+    <div class="container mt-3">
+        <div class="card mx-auto" style="width: 20rem;">
+
+            <img class="card-img-top" src="./images/login2.png" alt="login icon">
+
+            <div class="card-body">
+                <h5 class="card-title">Login</h5>
+                <form>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                            placeholder="Enter email">
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
+                            else.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+                    <button type="submit" class="btn btn-primary"><i class="fa fa-lock">&nbsp;</i>Login</button>
+                    <span> <a href="#">Register</a> </span>
+                </form>
+            </div>
+
+            <div class="card-footer">
+                <a href="#">Forgot Password?</a>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
