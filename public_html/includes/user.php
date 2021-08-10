82b666dd3fc0fd4a5cb17eda6a6ee71e0d9294d4
LilianMathu <lilianmathu2018@gmail.com>
2021-08-10 10:53:15 +0300

    print username

--- public_html/includes/user.php
+++ public_html/includes/user.php
@@ -96,5 +96,7 @@ $user = new User();
 echo $user->CreateUserAccount("test", "test@mail.com", "123456", "Other");
 
 echo $user->userLogin("lily@mail.com", "1234567890");
-   
+
+echo $_SESSION["username"];
+
 ?>
