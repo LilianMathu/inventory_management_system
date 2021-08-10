200742ef36d5d19ea55bd98588a038f71155af4e
LilianMathu <lilianmathu2018@gmail.com>
2021-08-10 11:48:05 +0300

    Add form validations

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
+    $("#registration_form").on("submit", function () {
+        var name = $("#username");
+        var email = $("#email");
+        var pass1 = $("#password1");
+        var pass2 = $("#password2");
+        var type = $("#userType");
+        
+        var u_patt = new RegExp(/^[A-Za-z ]+$/);
 
+        // lily@mail.com
+        var e_patt = new RegExp(/^[a-z0-9_-]+(\.[a-z0-9_-]+)*@[a-z0-9_-]+(\.[a-z0-9_-]+)*(\.[a-z]{2,4})$/);
+
+    })
 });
