0ac784c9dcfd7a302a0b28d8d961d0c27acb81ef
LilianMathu <lilianmathu2018@gmail.com>
2021-08-13 09:22:02 +0300

    validate registration fields

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,4 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    
+    var DOMAIN = "http//localhost/inventory_system/public_html/";
+
     $("#registration_form").on("submit", function() {
         var status = false;
         var name = $("#username");
@@ -30,6 +33,46 @@ $(document).ready(function() {
             email.removeClass("border-danger");
             $("#e_error").html("");
             status = true;
+        }
+        if (pass1.val() == "" || pass1.val().length < 8)  {
+            pass1.addClass("border-danger");
+            $("#p1_error").html("<span class='text-danger'>Password should be 8 digits</span>");
+            status = false;
+        } else {
+            pass1.removeClass("border-danger");
+            $("#p1_error").html("");
+            status = true;
+        }
+        if (pass2.val() == "" || pass2.val().length < 8)  {
+            pass2.addClass("border-danger");
+            $("#p2_error").html("<span class='text-danger'>Password should be 8 digits</span>");
+            status = false;
+        } else {
+            pass2.removeClass("border-danger");
+            $("#p2_error").html("");
+            status = true;
+        }
+        if (type.val() == "") {
+            type.addClass("border-danger");
+            $("#t_error").html("<span class='text-danger'>Please enter a user type</span>");
+            status = false;
+        } else {
+            type.removeClass("border-danger");
+            $("#t_error").html("");
+            status = true;
+        }
+        if (pass1.val() == pass2.val()) {
+            $.ajax({
+                url: DOMAIN + "includes.process.php",
+                method: "POST",
+                data: $("#registration_form").serialize(),
+                success: function (data) {
+                    alert(data);
+                }
+           })
+        } else {
+            pass2.addClass("border-danger");
+            $("#p2_error").html("<span>Password do not match</span>");
         }
 
     })
